a8f31b0417e8db93c0c2a199dae149f00790b748
Bamacharan Kundu <bamachrn@gmail.com>
2017-11-07 12:58:03 +0530

    updated tracking urls

--- trigger_pre_build.py
+++ trigger_pre_build.py
@@ -2,10 +2,10 @@ import os
 import yaml
 
 
-project={}
-project['project_git_url']="https://github.com/container-images/tools"
-project['project_git_branch']="master"
-project['script_path']="hooks/pre_build_centos"
+project=[{'project':{}}]
+project[0]['project']['project_git_url']="https://github.com/container-images/tools"
+project[0]['project']['project_git_branch']="master"
+project[0]['project']['script_path']="hooks/pre_build_centos"
 
 with open('pre-build-test.yaml','w') as outfile:
     yaml.dump(project,outfile)
